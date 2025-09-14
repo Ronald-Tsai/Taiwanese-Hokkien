4414efeb8b98f3c5ba95a2e0da2c089df43dda5b
Ronald Tsai <tsai210@purdue.edu>
2025-09-14 12:40:09 -0400

    set up React skeleton

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "sans-serif" }}>
+      <h1>Hokkien Translator</h1>
+      <p>Welcome!</p>
+    </div>
+  );
+}
+
+export default App;
